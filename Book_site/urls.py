5d57a18d776ffe810b122d57ac36e684a7b6f981
lukasz <uhenzel@gmail.com>
2024-02-16 14:18:40 +0100

    Your shelf mechanics added

--- Book_site/urls.py
+++ Book_site/urls.py
@@ -6,4 +6,6 @@ urlpatterns = [
     path('', views.MainPageView.as_view(), name="main-page"),
     path('book/<slug:slug>', views.BookDetailView.as_view(), name="book-detail"),
     path('searched', views.SearchedView.as_view(), name="searched"),
+    path('shelf', views.AddToShelfView.as_view(), name="shelf"),
+
 ]
--- Book_site/views.py
+++ Book_site/views.py
@@ -3,6 +3,8 @@ from django.views.generic import DetailView, ListView
 from .models import Book
 from django.views import View
 from django.db.models import Q
+from django.http import HttpResponseRedirect
+from django.urls import reverse
 # Create your views here.
 
 
@@ -32,3 +34,37 @@ class SearchedView(View):
             "searched_books": searched_books,
         }
         return render(request, "book_site/searched.html", context)
+
+
+class AddToShelfView(View):
+    def get(self, request):
+        stored_books = request.session.get("stored_books")
+
+        context = {}
+
+        if stored_books is None or len(stored_books) == 0:
+            context["books"] = []
+            context["has_books"] = False
+        else:
+            books = Book.objects.filter(id__in=stored_books)
+            context["books"] = books.order_by("title")
+            context["has_books"] = True
+
+        return render(request, "book_site/shelf.html", context)
+
+    def post(self, request):
+        stored_books = request.session.get("stored_books")
+
+        if stored_books is None:
+            stored_books = []
+
+        book_id = int(request.POST["book_id"])
+
+        if book_id not in stored_books:
+            stored_books.append(book_id)
+        else:
+            stored_books.remove(book_id)
+
+        request.session["stored_books"] = stored_books
+
+        return HttpResponseRedirect("shelf")
