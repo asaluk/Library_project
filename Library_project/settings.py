5cd3ffcddc837ee77812b1b208e16d5e2fc200e6
lukasz <uhenzel@gmail.com>
2024-04-06 15:52:47 +0200

    r

--- Library_project/settings.py
+++ Library_project/settings.py
@@ -120,7 +120,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
 STATIC_ROOT = BASE_DIR / "staticfiles"
-STATIC_URL = "static/"
+STATIC_URL = "/static/"
 
 STATICFILES_DIRS = [
     BASE_DIR / "static"
